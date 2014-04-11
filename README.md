ef99ed88b6af8154ca69089a368c219fd01ec496
Ash Kyd <ash@kyd.com.au>
2014-04-12 01:31:51 +1000

    Initial commit + docs.

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 wzkeygen
 ========
 
-Weatherzone keygen: creates your rolling key for the Weatherzone API.
+Implements the [Weatherzone](http://www.weatherzone.com.au/) date-based rolling key for accessing the API.
+
+Specification
+-------------
+The Wetherzone API uses a custom rolling key for authentication. This rolling key is valid for 48 hours of the date in question and should be regenerated each day.
+
+The key is defined as a MD5summed hash of the salt and key concatenated.
+
+The salt is a number defined as the sum of day*2, month*300, year*1700000.
+
+Usage
+-----
+Install wzkeygen as a NPM module, then:
+
+    var wzkeygen = require('wzkeygen');
+    var myKey = wzKeygen('mypassword');
+
+Bugs
+----
+This is really simple but please feel free to submit a pull request if this 
+doesn't work for you.
---
+++ index.js
@@ -0,0 +1,27 @@
+var md5 = require('MD5');
+
+/**
+ * Weatherzone keygen
+ * Implements the Weatherzone date-based rolling key for accessing the API.
+ * The salt is defined as the sum of day*2, month*300, year*1700000.
+ * The rolling key is defined as the MD5 sum of the salt and password conatenated.
+ * @param  {string} password Your secret password to hash into a rolling key.
+ * @return {string}          MD5 key used to fetch data from Weatherzone.
+ */
+module.exports = function(password){
+	var salt, now, day, month, year;
+
+	// Right now
+	now = new Date();
+
+	// Pick out our day, month & two digit year.
+	day = now.getDay();
+	month = now.getMonth()+1;
+	year = now.getYear().toString().substring(2);
+
+	// Create the salt as defined.
+	salt = day*2 + month*300 + year*1700000;
+
+	// Return the salted and hashed secret.
+	return md5(salt+password);
+}
